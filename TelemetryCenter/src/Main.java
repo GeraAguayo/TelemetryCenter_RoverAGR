ecb43a75c6c75a65776eb4495fe55edae783e421
Gerardo Aguayo <geraaguayoc@gmail.com>
2025-07-23 19:21:20 -0600

    Sensor values set on interface

--- TelemetryCenter/src/Main.java
+++ TelemetryCenter/src/Main.java
@@ -1,6 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        MainWindow app = new MainWindow();
-        app.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            MainWindow app = new MainWindow();
+            app.setVisible(true);
+        });
     }
 }
--- TelemetryCenter/src/MainWindow.java
+++ TelemetryCenter/src/MainWindow.java
@@ -1,22 +1,28 @@
 import javax.swing.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
     private JPanel MainPanel;
-    private JPanel section1;
-    private JPanel section2;
-    private JPanel section3;
-    private JPanel datetimePanel;
-    private JPanel logoPanel;
+    private JLabel datetimeLabel;
+    private JLabel logoLabel;
     private JPanel videoPanel;
     private JPanel sensorPanel;
     private JPanel chartPanel;
     private JPanel logsPanel;
-    private JLabel datetimeTitle;
-    private JLabel logoTitle;
-    private JLabel videoTitle;
-    private JLabel sensorTitle;
-    private JLabel chartTitle;
-    private JLabel logsTitle;
+    private JLabel titleSensor;
+    private JLabel labelTitleTemp;
+    private JLabel labelTitleAlt;
+    private JLabel labelTitlePres;
+    private JLabel labelTemp;
+    private JLabel labelAlt;
+    private JLabel labelPress;
+    private JTextField inputIP;
+    private JButton buttonIP;
+
+    Date date_info = new Date();
 
     public MainWindow(){
         this.setTitle("Telemetry AGR");
@@ -26,5 +32,32 @@ public class MainWindow extends JFrame {
         this.pack();
         this.setSize(1920,1080);
         this.setLocationRelativeTo(null);
+
+        //Initialize datetime timer
+        startDateTimeUpdater();
     }
+
+    private void updateDateTime() {
+        Date now = new Date(); // Get the current date and time
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = dateFormat.format(now);
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+        String timeString = timeFormat.format(now);
+
+        this.datetimeLabel.setText("Date: " + dateString + "   Time: " + timeString);
+    }
+
+    private void startDateTimeUpdater() {
+        Timer timer = new Timer(5000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateDateTime();
+            }
+        });
+        timer.setInitialDelay(0);
+        timer.start();
+    }
+
 }
