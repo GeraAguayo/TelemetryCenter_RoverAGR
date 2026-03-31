b78844e8d10d3371d7196dec7754d2e93ff8a454
Gerardo Aguayo <geraaguayoc@gmail.com>
2026-03-31 13:04:22 -0600

    Changed timeout to 10 secs

--- TelemetryCenter/src/UDP.java
+++ TelemetryCenter/src/UDP.java
@@ -17,10 +17,6 @@ public class UDP {
     float gas = 0.0f;
     float lat = 0.0f;
     float lon = 0.0f;
-    //Current logs
-    //public String LOG_TXT = "";
-    //static int MAX_LOG_DISPLAY = 5;
-    //Queue<String> log_queue = new ArrayDeque<>(MAX_LOG_DISPLAY);
     Syslog syslog_dict = new Syslog();
     //Control creation of multiple sockets
     boolean SOCKET_OPEN = false;
@@ -37,9 +33,9 @@ public class UDP {
         Scanner sc = new Scanner(System.in);
         this.ip = InetAddress.getByName(ip_address);
         close();
-        //create socket with 8 segs of timeout
+        //create socket with 10 segs of timeout
         this.socket = new DatagramSocket(PORT);
-        this.socket.setSoTimeout(8000);
+        this.socket.setSoTimeout(10000);
         this.SOCKET_OPEN = true;
         updateLog(CONNECTION_LOG);
 
@@ -56,8 +52,6 @@ public class UDP {
 
     //Update System logs queue
     void updateLog(int log_id){
-        
-
         //check for size
         if (MainWindow.log_queue.size() >= MainWindow.MAX_LOG_DISPLAY){
             MainWindow.log_queue.poll();
